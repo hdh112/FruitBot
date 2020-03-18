8915f63f69be8edf8cd9859144d451fec8839139
hdh112 <avohdh112@gmail.com>
2020-03-18 17:28:17 +0900

    Labeled data and encoded vocabulary(Fixed runtime errors)

--- classifying_model.py
+++ classifying_model.py
@@ -1,44 +1,44 @@
-# Reference: https://www.tensorflow.org/tutorials/load_data/text
+# Reference: 	https://www.tensorflow.org/tutorials/load_data/text
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 import tensorflow_datasets as tfds
-# import os
+import numpy as np
 
-DIRECTORY = 'chunks/'
-FILE_NAMES = ['[chunk]fruit_order.txt']
+DIRECTORY = 'words/'
+FILE_NAMES = ['[words]fruit_order.txt']
 vocabulary_set = set()
 
 # TODO: make labels more specific, according to tags and intent
 # Label 0: order
 def labeler(sentence, index):
-	untagged_words = tagged_word.split('/')[0] \
-						for tagged_word in sentence.rstrip().split()
-	vocabulary_set.update(untagged_words)
-
-	return " ".join(untagged_words), tf.cast(index, tf.int64)
+	return sentence, tf.cast(index, tf.int64)
 
 labeled_data_sets = []
 
 for i, file_name in enumerate(FILE_NAMES):
-	# lines_dataset = tf.data.TextLineDataset(tf.cast(DIRECTORY + file_name, tf.string))
 	lines_dataset = tf.data.TextLineDataset(DIRECTORY + file_name)
-	labeled_dataset = lines_dataset.map(lambda stnc: labeler(stnc, i))
+	# TODO: iterate lines_dataset only once;
+	# 		for now, iterates twice because of numpy-tensorflow compatability issue
+	labeled_dataset = lines_dataset.map(lambda sent: labeler(sent, i))
+	for line in lines_dataset.as_numpy_iterator():
+		# TODO: handle multiple meanings of a single word
+		vocabulary_set.update(line.rstrip().split())
+			
 	labeled_data_sets.append(labeled_dataset)
 
-# print(len(vocabulary_set))
-# TODO: handle regex "\W+" that comes along with Korean(non-alphanumeric) characters
+print(len(vocabulary_set))
+print(vocabulary_set)
 encoder = tfds.features.text.TokenTextEncoder(vocabulary_set)
+
+all_labeled_data = labeled_data_sets[0]
+for labeled_dataset in labeled_data_sets[1:]:
+	all_labeled_data = all_labeled_data.concatenate(labeled_dataset)
 
 # TODO: make dataset larger
 BUFFER_SIZE = 200
 BATCH_SIZE = 16
 TAKE_SIZE = 20
 
-all_labeled_data = labeled_data_sets[0]
-for labeled_dataset in labeled_data_sets[1:]:
-	all_labeled_data = all_labeled_data.concatenate(labeled_dataset)
-
 # all_labeled_data = all_labeled_data.shuffle(BUFFER_SIZE, reshuffle_each_iteration=True)
-all_labeled_data = all_labeled_data.shuffle(BUFFER_SIZE, reshuffle_each_iteration=False)
-
+# all_labeled_data = all_labeled_data.shuffle(BUFFER_SIZE, reshuffle_each_iteration=False)
